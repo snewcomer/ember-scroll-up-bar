3d8f133dac461a368208434fce646725ba040514
snewcomer24 <snewcomer24@gmail.com>
2018-01-11 07:27:54 -0800

    add requestAnimationFrame

--- addon/components/ember-scroll-up-bar.js
+++ addon/components/ember-scroll-up-bar.js
@@ -1,14 +1,11 @@
 import Component from '@ember/component';
-import { get } from '@ember/object';
+import { get, set } from '@ember/object';
+import { bind } from '@ember/runloop';
 
 export default Component.extend({
-  classNames: ['ember-scroll-up-bar', 'animated-fast'],
-  // classNameBindings: ['showMe:ember-scroll-up-bar--show'],//, 'slideInDown:slideInDown', 'slideOutUp:slideOutUp'],
+  classNames: ['ember-scroll-up-bar'],
 
-  // /**
-  //  * @property showMe
-  //  */
-  // showMe: null,
+  _scheduledAnimationFrame: false,
   /**
    * user specified from top of document
    * @property topPos
@@ -17,12 +14,11 @@ export default Component.extend({
 
   _originalPosTop: 0,
 
-  slideTimer: 400,
-
   didInsertElement() {
     this.top = window.pageYOffset || document.documentElement.scrollTop;
     this._originalPosTop = this.element.getBoundingClientRect().top;
     this.element.style.top = get(this, 'topPos') || this._originalPosTop;
+    // if start out in middle of page
     this.cachedTop = `${this.top}px`;
     this._originalBottom = this.element.offsetHeight + this.element.offsetTop;
 
@@ -36,30 +32,49 @@ export default Component.extend({
    * @method _scrollClosure
    */
   _scrollClosure() {
+    if (this._scheduledAnimationFrame) return;
+
+    // read before requestAnimationFrame to avoid layout thrashing
     let newTop = window.pageYOffset || document.documentElement.scrollTop;
 
     // reset
     if (newTop === 0) {
-      this.element.style.position = 'absolute';
-      this.element.style.top = this._originalPosTop;
+      set(this, '_scheduledAnimationFrame', true);
+      // requestAnimationFrame to run visual changes in order to not block scrolling/touch events
+      window.requestAnimationFrame(bind(this, this._resetBar, newTop));
     }
 
     // scrolled down
     if (newTop > this.top) {
-      this._scrolledDown(newTop);
+      set(this, '_scheduledAnimationFrame', true);
+      // requestAnimationFrame to run visual changes in order to not block scrolling/touch events
+      window.requestAnimationFrame(bind(this, this._scrolledDown, newTop));
     }
 
     // scrolled up
     if (newTop < this.top) {
       // ensure not in container of header
       if (this._originalBottom < newTop) {
-        this.element.style.position = 'fixed';
-        this.element.style.top = '0px';
-        this.cachedTop = `${newTop}px`;
+        set(this, '_scheduledAnimationFrame', true);
+        // requestAnimationFrame to run visual changes in order to not block scrolling/touch events
+        window.requestAnimationFrame(bind(this, this._scrolledUp, newTop));
       }
     }
 
     this.top = newTop;
+  },
+
+  _resetBar() {
+    this.element.style.position = 'absolute';
+    this.element.style.top = this._originalPosTop;
+    set(this, '_scheduledAnimationFrame', false);
+  },
+
+  _scrolledUp(newTop) {
+    this.element.style.position = 'fixed';
+    this.element.style.top = '0px';
+    this.cachedTop = `${newTop}px`;
+    set(this, '_scheduledAnimationFrame', false);
   },
 
   _scrolledDown(newTop) {
@@ -69,6 +84,7 @@ export default Component.extend({
     } else if (this._originalBottom * 2 < newTop) {
       this.element.style.top = this.cachedTop;
     }
+    set(this, '_scheduledAnimationFrame', false);
   },
 
   willDestroyElement() {
